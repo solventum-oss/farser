d17e2bfa2c4a0dfe98bb7f97f8f9966b66a49c94
Thomas Naeff <tnaeff@mmm.com>
2022-08-02 13:01:40 -0400

    AST syntax tree printing

--- src/main/java/com/mmm/his/cer/utility/farser/ast/DrgSyntaxTree.java
+++ src/main/java/com/mmm/his/cer/utility/farser/ast/DrgSyntaxTree.java
@@ -1,6 +1,7 @@
 package com.mmm.his.cer.utility.farser.ast;
 
 import com.mmm.his.cer.utility.farser.ast.node.type.BooleanExpression;
+import com.mmm.his.cer.utility.farser.ast.node.type.ExpressionIterator;
 import com.mmm.his.cer.utility.farser.ast.parser.ExpressionResult;
 
 /**
@@ -32,4 +33,69 @@ public class DrgSyntaxTree<T> {
     boolean evaluate = this.ast.evaluate(context);
     return new ExpressionResult<>(evaluate, context);
   }
+
+  /**
+   * Prints a simple tree representation.
+   *
+   * @return The tree representation as string
+   */
+  public String printTree() {
+    return printTree(null);
+  }
+
+  /**
+   * Prints a simple tree representation.
+   *
+   * @param context the context object that will be used in the evaluation. May be <code>null</code>
+   *                to only print the nodes and not print the evaluation result of each)
+   * @return The tree representation as string
+   */
+  public String printTree(C context) {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(printNode(this.ast, context));
+    sb.append(System.lineSeparator());
+
+    ExpressionIterator<C> iter = this.ast.iterator();
+    while (iter.hasNext()) {
+      BooleanExpression<C> node = iter.next();
+      sb.append(prefix(iter.getCurrentDepth()));
+      sb.append(printNode(node, context));
+      sb.append(System.lineSeparator());
+    }
+
+    return sb.toString();
+  }
+
+  /**
+   * Prints the output of a single node.
+   *
+   * @param node    The node to print
+   * @param context The context for node evaluation. May be <code>null</code> to skip evaluation
+   * @return The node output
+   */
+  private static <C> StringBuilder printNode(BooleanExpression<C> node, C context) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(String.valueOf(node.print()));
+    if (context != null) {
+      boolean result = node.evaluate(context);
+      sb.append(" = " + result);
+    }
+    return sb;
+  }
+
+  /**
+   * Generates a prefix to structure each line of the tree.
+   *
+   * @param depth The prefix width
+   * @return The prefix
+   */
+  private static String prefix(int depth) {
+    StringBuilder sb = new StringBuilder();
+    for (; depth > 0; depth--) {
+      sb.append("  ");
+    }
+    return sb.toString();
+  }
+
 }
